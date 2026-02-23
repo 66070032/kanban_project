2366b810f677df3a923e7c3ebc55c5b4091dd7d9
66070032 <66070032@kmitl.ac.th>
2026-02-23 17:05:59 +0700

    Test timezone

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,10 +4,10 @@ const cors = require('cors');
 const app = express();
 const morgan = require('morgan');
 
-app.use(morgan('dev'));
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'));
 
 const userRoutes = require('./routes/user.routes');
 const taskRoutes = require('./routes/task.routes');
